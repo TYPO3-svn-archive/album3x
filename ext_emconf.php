eb7948d2bb66e4b332461736246ecc2a5bf7b609
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-16 07:31:23 +0000

    Small formatting changes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/album3x/trunk@14957 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "album3x"
 #
-# Auto generated 17-10-2007 11:01
+# Auto generated 18-12-2007 09:44
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => '3X Album',
-	'description' => 'Image album (photo gallery) with thumbnails, mid-size and click-open full size photos',
+	'description' => 'Image album (photo gallery) with thumbnails, mid-size and click-open full size photos. Public free support is provided only through TYPO3 mailing lists! Contact by e-mail for commercial support.',
 	'category' => 'plugin',
 	'author' => 'Dmitry Dulepov',
 	'author_email' => 'dmitry@typo3.org',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"565d";s:31:"class.tx_album3x_cms_layout.php";s:4:"72e9";s:28:"class.tx_album3x_realurl.php";s:4:"ef77";s:12:"ext_icon.gif";s:4:"6268";s:17:"ext_localconf.php";s:4:"7bc4";s:14:"ext_tables.php";s:4:"3c14";s:14:"ext_tables.sql";s:4:"6f35";s:26:"icon_tx_album3x_images.gif";s:4:"3153";s:13:"locallang.xml";s:4:"1254";s:17:"locallang_csh.xml";s:4:"0a00";s:16:"locallang_db.xml";s:4:"f8fb";s:7:"tca.php";s:4:"cf37";s:14:"doc/manual.sxw";s:4:"0a6c";s:14:"pi1/ce_wiz.gif";s:4:"8894";s:28:"pi1/class.tx_album3x_pi1.php";s:4:"6729";s:36:"pi1/class.tx_album3x_pi1_wizicon.php";s:4:"cd01";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"3f5e";s:17:"pi1/locallang.xml";s:4:"1296";s:11:"res/pi1.css";s:4:"fb3c";s:12:"res/pi1.html";s:4:"643c";s:29:"static/3X_Album/constants.txt";s:4:"d41d";s:25:"static/3X_Album/setup.txt";s:4:"9b99";}',
+	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"c504";s:31:"class.tx_album3x_cms_layout.php";s:4:"72e9";s:28:"class.tx_album3x_realurl.php";s:4:"bf04";s:12:"ext_icon.gif";s:4:"6268";s:17:"ext_localconf.php";s:4:"7bc4";s:14:"ext_tables.php";s:4:"3c14";s:14:"ext_tables.sql";s:4:"6f35";s:26:"icon_tx_album3x_images.gif";s:4:"3153";s:13:"locallang.xml";s:4:"1254";s:17:"locallang_csh.xml";s:4:"0a00";s:16:"locallang_db.xml";s:4:"f8fb";s:7:"tca.php";s:4:"cf37";s:14:"doc/manual.sxw";s:4:"0a6c";s:14:"pi1/ce_wiz.gif";s:4:"8894";s:28:"pi1/class.tx_album3x_pi1.php";s:4:"1169";s:36:"pi1/class.tx_album3x_pi1_wizicon.php";s:4:"cd01";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"3f5e";s:17:"pi1/locallang.xml";s:4:"1296";s:11:"res/pi1.css";s:4:"fb3c";s:12:"res/pi1.html";s:4:"643c";s:29:"static/3X_Album/constants.txt";s:4:"d41d";s:25:"static/3X_Album/setup.txt";s:4:"9b99";}',
 	'suggests' => array(
 	),
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -10,5 +10,6 @@ t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_album3x_pi1.php','_pi1','list_t
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['album3x_pi1'][] = 'EXT:album3x/class.tx_album3x_cms_layout.php:tx_album3x_cms_layout->getExtensionSummary';
 
 // RealURL autoconfiguration
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['album3x'] = 'EXT:album3x/class.tx_album3x_realurl.php:tx_album3x_realurl->addAlbum3xConfig'
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['album3x'] = 'EXT:album3x/class.tx_album3x_realurl.php:tx_album3x_realurl->addAlbum3xConfig';
+
 ?>
